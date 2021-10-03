a093d1d31566debba39e4280d896bd96830ad780
John Watson <johnwatson484@hotmail.com>
2021-10-03 21:32:44 +0100

    Refactor

--- index.js
+++ index.js
@@ -1,52 +1,50 @@
-const { Engine } = require('json-rules-engine')
-const engine = new Engine()
+(async function () {
+  const { Engine } = require('json-rules-engine')
+  const engine = new Engine()
 
-// define a rule for detecting the player has exceeded foul limits.  Foul out any player who:
-// (has committed 5 fouls AND game is 40 minutes) OR (has committed 6 fouls AND game is 48 minutes)
-engine.addRule({
-  conditions: {
-    any: [{
-      all: [{
-        fact: 'gameDuration',
-        operator: 'equal',
-        value: 40
+  // define a rule for detecting the player has exceeded foul limits.  Foul out any player who:
+  // (has committed 5 fouls AND game is 40 minutes) OR (has committed 6 fouls AND game is 48 minutes)
+  engine.addRule({
+    conditions: {
+      any: [{
+        all: [{
+          fact: 'gameDuration',
+          operator: 'equal',
+          value: 40
+        }, {
+          fact: 'personalFoulCount',
+          operator: 'greaterThanInclusive',
+          value: 5
+        }]
       }, {
-        fact: 'personalFoulCount',
-        operator: 'greaterThanInclusive',
-        value: 5
+        all: [{
+          fact: 'gameDuration',
+          operator: 'equal',
+          value: 48
+        }, {
+          fact: 'personalFoulCount',
+          operator: 'greaterThanInclusive',
+          value: 6
+        }]
       }]
-    }, {
-      all: [{
-        fact: 'gameDuration',
-        operator: 'equal',
-        value: 48
-      }, {
-        fact: 'personalFoulCount',
-        operator: 'greaterThanInclusive',
-        value: 6
-      }]
-    }]
-  },
-  event: { // define the event to fire when the conditions evaluate truthy
-    type: 'fouledOut',
-    params: {
-      message: 'Player has fouled out!'
+    },
+    event: { // define the event to fire when the conditions evaluate truthy
+      type: 'fouledOut',
+      params: {
+        message: 'Player has fouled out!'
+      }
     }
-  }
-})
+  })
 
-/**
+  /**
  * Define facts the engine will use to evaluate the conditions above.
  * Facts may also be loaded asynchronously at runtime; see the advanced example below
  */
-const facts = {
-  personalFoulCount: 6,
-  gameDuration: 40
-}
+  const facts = {
+    personalFoulCount: 6,
+    gameDuration: 40
+  }
 
-// Run the engine to evaluate
-engine
-  .run(facts)
-  .then(({ events }) => {
-    events.map(event => console.log(event.params.message))
-  })
+  const result = await engine.run(facts)
+  result.events.map(event => console.log(event.params.message))
+}())
